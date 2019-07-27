01268b73c2146a8a20566b6454d636efcea00c63
mordi244 <mordi244@gmail.com>
2019-07-27 11:21:40 +0300

    last updata

--- index.js
+++ index.js
@@ -15,54 +15,48 @@ const getDataFromAPI = async (userName) => {
 
 //create html elements for the main page
 const createDivsMain = (content) => {
-    //
     let divimg = document.createElement("div");
-    let divh = document.createElement("div");
-    let mainDiv222 = document.createElement("div");
-
-
-    //
-    // create html elements
+    let divh1 = document.createElement("div");
+    // create and set html elements
     let container = document.querySelector(".container");
     let userContainer = document.createElement("div");
     let userImg = document.createElement("img");
     let name = document.createElement("h4");
     let btnRepo = document.createElement('button');
     let btnProfile = document.createElement('button');
-    divh.appendChild(name);
+    divh1.appendChild(name);
     btnRepo.textContent = "Repositories";
     btnProfile.textContent = "Full Profile";
     name.textContent = content.login; //get header by repository login owner
     userContainer.setAttribute("class", "container-user");
     userImg.setAttribute("src", content.avatar_url);
-    divimg.appendChild(userImg);
     //append child to container
+    divimg.appendChild(userImg);
+    userContainer.appendChild(divh1);
     userContainer.appendChild(divimg);
     userContainer.appendChild(name);
     userContainer.appendChild(btnRepo);
     userContainer.appendChild(btnProfile);
+    container.appendChild(userContainer);
     
-
     //click listener for loading repositories
     btnRepo.addEventListener("click", function () {
         container.innerHTML = "";
         getDataRepo(content);
     });
-    btnProfile.addEventListener("click",function() {
-        console.log("fdfsdf");
+    //click listener for loading profile site
+    btnProfile.addEventListener("click", function () {
         window.location = content.html_url;
     });
-
-    container.appendChild(userContainer);
 }
 
-
+//get repositories for user
 const getDataRepo = async (content) => {
     try {
+        //get data from api
         let response = await fetch(content.repos_url);
         let myJson = await response.json();
-        //creating html elements
-        console.log(myJson);
+        //creating and set html elements
         let container = document.querySelector(".container");
         let headerDiv = document.createElement("div");
         let h1Div = document.createElement("div");
@@ -70,11 +64,11 @@ const getDataRepo = async (content) => {
         let mainHeader = document.createElement("h1");
         let userImg = document.createElement("img");
         userImg.setAttribute("src", content.avatar_url);
-        mainHeader.textContent = content.login +' Repositories ('+myJson.length +')';
+        mainHeader.textContent = content.login + "'s "+ ' Repositories (' + myJson.length + ')';
         headerDiv.setAttribute("class", "divHeader");
+        //append child to container
         imgDiv.appendChild(userImg);
         h1Div.appendChild(mainHeader);
-        //append child to container
         headerDiv.appendChild(imgDiv);
         headerDiv.appendChild(h1Div);
         container.appendChild(headerDiv);
@@ -82,13 +76,14 @@ const getDataRepo = async (content) => {
         for (let i = 0; i < myJson.length; i++) {
             createDivsRepos(myJson[i], content.repos_url);
         }
-
     } catch (err) {
         console.log(err);
     }
 }
+
+//create ui for user repositories
 function createDivsRepos(content, url) {
-    //creating html elements
+    //creating and set html elements
     let container = document.querySelector(".container");
     let userContainer = document.createElement("div");
     let headerDiv = document.createElement("div");
@@ -123,6 +118,5 @@ document.querySelector("#search").addEventListener('click', function () {
     let username = document.querySelector("#username");
     let container = document.querySelector(".container");
     container.innerHTML = ""; // clean page berfore load html
-    console.log(username.value)
     getDataFromAPI(username);
 });
